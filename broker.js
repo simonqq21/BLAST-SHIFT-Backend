3c4850f5cbf7577898725eb3eab544b235b9f1c8
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-05-05 20:19:40 +0800

    topic length edit

--- broker.js
+++ broker.js
@@ -221,6 +221,8 @@ function parseData(raw_data, location) {
 function checkPacketTopic(topic) {
   let topicsplitted = topic.split('/');
    // If invalid format
+  console.log("topic: " + topic)
+  console.log("Length: " + topicsplitted.length)
   if(topicsplitted.length == 6) 
     return true
   else
